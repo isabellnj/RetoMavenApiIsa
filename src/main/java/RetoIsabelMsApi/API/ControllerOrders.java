1104f7a0b97f099156141b8b42eb174d82864ec9
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2020-12-07 02:42:31 +0100

    ejercicio4

--- src/main/java/RetoIsabelMsApi/API/ControllerOrders.java
+++ src/main/java/RetoIsabelMsApi/API/ControllerOrders.java
@@ -60,82 +60,82 @@ public class ControllerOrders {
     //Devuelve la información de un pedido concreto y sus productos asociados a través del ID del pedido.
 
    
-   @GetMapping("/order/{id}")
-   public static OrderProductFull orderById(@PathVariable("id") final int id) {
-      OrderProduct o1= new OrderProduct();
-      int cant = 0;
-       for (final Order order: listado){
-           if (order.getId() == id){
-               ArrayList<Product> products= new ArrayList<Product>();
-               
-               for (final OrderProduct orderProduct : ControllerOrderProduct.listado) {
-                   if (orderProduct.getOrder().getId() == id) {
-                       
-
-                       products.add(orderProduct.getProduct());
-                       cant = orderProduct.getQuantity();
-                       
-                    
-                   }
-               }
-               return new OrderProductFull(order, products, cant );
-               
-           }
+    @GetMapping("/order/{id}")
+    public static OrderProductFull orderById(@PathVariable("id") final int id) {
+       OrderProduct o1= new OrderProduct();
+       int cant = 0;
+        for (final Order order: listado){
+            if (order.getId() == id){
+                ArrayList<Product> products= new ArrayList<Product>();
+                
+                for (final OrderProduct orderProduct : ControllerOrderProduct.listado) {
+                    if (orderProduct.getOrder().getId() == id) {
+                        
+ 
+                        products.add(orderProduct.getProduct());
+                        cant = orderProduct.getQuantity();
+                        
+                     
+                    }
+                }
+                return new OrderProductFull(order, products, cant );
+                
+            }
+         }
+ 
+         throw new ElementNotFound();
+        
+    }
+ 
+ 
+ 
+    //-	Permite insertar un pedido con los productos y la cantidad de cada producto deseada. Se tendrá que devolver, 
+    //al menos, el ID en la petición para poder comprobar que toda la información se ha insertado correctamente.
+ 
+    @PostMapping("/orderp")
+    public int add(@RequestBody OrderFull order) {
+        Order o = new Order(order.getDate(), order.getName(), order.getState());
+        listado.add(o);
+        for (Map.Entry<Integer, Integer> producto : order.getProductos().entrySet()) {
+            Product p = ControllerProducts.getById(producto.getKey());
+            OrderProduct op = new OrderProduct(o, p, producto.getValue());
+            ControllerOrderProduct.listado.add(op);
         }
-
-        throw new ElementNotFound();
-       
-   }
-
-
-
-   //-	Permite insertar un pedido con los productos y la cantidad de cada producto deseada. Se tendrá que devolver, 
-   //al menos, el ID en la petición para poder comprobar que toda la información se ha insertado correctamente.
-
-   @PostMapping("/orderp")
-   public int add(@RequestBody OrderFull order) {
-       Order o = new Order(order.getDate(), order.getName(), order.getState());
-       listado.add(o);
-       for (Map.Entry<Integer, Integer> producto : order.getProductos().entrySet()) {
-           Product p = ControllerProducts.getById(producto.getKey());
-           OrderProduct op = new OrderProduct(o, p, producto.getValue());
-           ControllerOrderProduct.listado.add(op);
-       }
-
-      return o.getId();
-   }
-
-
-   //-	Permite eliminar un pedido. Al eliminar un pedido también se eliminarán los productos vinculados a ese pedido.
-
-   @DeleteMapping("/orders/{id}")
-   public static Order getById(@PathVariable("id") final int id) {
-       for (Order order : listado) {
-           if (order.getId() == id) {
-               for (int i = 0; i < ControllerOrderProduct.listado.size(); i++) {
-                   OrderProduct orderProduct = ControllerOrderProduct.listado.get(i);
-                   if (orderProduct.getOrder().getId() == id) {
-                       ControllerOrderProduct.listado.remove(orderProduct);
-                   }
-               }
-               listado.remove(order);
-               return null;
-           }
-       }
-
-       return null;
-   }
-
-
-   ///-	En caso de que se proporcione un ID que no exista o haya algún error, 
-   //la petición debería devolver un error HTTP que identifique adecuadamente este error.
-
-   @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "id no existe")
-   public static class ElementNotFound extends RuntimeException{
-
-   }
-  
+ 
+       return o.getId();
+    }
+ 
+ 
+    //-	Permite eliminar un pedido. Al eliminar un pedido también se eliminarán los productos vinculados a ese pedido.
+ 
+    @DeleteMapping("/orders/{id}")
+    public static Order getById(@PathVariable("id") final int id) {
+        for (Order order : listado) {
+            if (order.getId() == id) {
+                for (int i = 0; i < ControllerOrderProduct.listado.size(); i++) {
+                    OrderProduct orderProduct = ControllerOrderProduct.listado.get(i);
+                    if (orderProduct.getOrder().getId() == id) {
+                        ControllerOrderProduct.listado.remove(orderProduct);
+                    }
+                }
+                listado.remove(order);
+                return null;
+            }
+        }
+ 
+        return null;
+    }
+ 
+ 
+    ///-	En caso de que se proporcione un ID que no exista o haya algún error, 
+    //la petición debería devolver un error HTTP que identifique adecuadamente este error.
+ 
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "id no existe")
+    public static class ElementNotFound extends RuntimeException{
+ 
+    }
    
+    
 
 
 
