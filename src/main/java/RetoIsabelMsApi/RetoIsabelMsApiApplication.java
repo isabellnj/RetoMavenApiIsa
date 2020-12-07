f50db2a72432aa5b8c18d52639f4647fa0a9a902
DESKTOP-3M2FJBS\USUARIO <isabellanaja0@gmail.com>
2020-12-07 02:05:45 +0100

    commit1

---
+++ src/main/java/RetoIsabelMsApi/RetoIsabelMsApiApplication.java
@@ -0,0 +1,13 @@
+package RetoIsabelMsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoIsabelMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoIsabelMsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RetoIsabelMsApi/RetoIsabelMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package RetoIsabelMsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoIsabelMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
